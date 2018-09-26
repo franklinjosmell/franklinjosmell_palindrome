7d7bc9c11c429e6529bdf609995ff364ccd1cd20
Franklin Ticona <franklinjosmell@gmail.com>
2018-09-25 21:57:09 -0500

    Finish the first version of palindrome method

--- lib/franklinjosmell_palindrome.rb
+++ lib/franklinjosmell_palindrome.rb
@@ -3,3 +3,17 @@ require "franklinjosmell_palindrome/version"
 module FranklinjosmellPalindrome
   # Your code goes here...
 end
+
+
+class String
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+  
+  private
+  
+    def processed_content
+      self.scan(/[a-z]/i).join.downcase
+      #self.downcase.scan(/[a-z]/).join
+    end
+end
